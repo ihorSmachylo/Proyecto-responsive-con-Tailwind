958b7bf5501d2232e1fb92fb2e9e0c09d455ce12
Ihor Smachylo <igoor.smachyloo@gmail.com>
2023-06-01 10:05:28 +0200

    proyecto de HTML I TAILWIND responsive

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,10 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {},
+    fontFamily:{
+      'sans': ['Inter var', 'sans-serif'],
+      'prueba':['Monomaniac One']
+    }
   },
   plugins: [],
 }
